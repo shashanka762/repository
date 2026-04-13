28aacfed48cc9c7b81a7d6374fedc96ca095e8b2
Shashank <sshashankams@gmail.com>
2026-04-13 12:55:34 +0530

    Add files via upload

---
+++ break_demo.java
@@ -0,0 +1,16 @@
+//program to demonstrate break statement using for loop
+class break_demo
+{
+	public static void main(String args[])
+	{
+		int i;
+		for (i=1;1<=10;i++)
+		{
+			System.out.println(i);
+			if(i==5)
+			{
+				break;
+			}
+		}
+	}
+}
